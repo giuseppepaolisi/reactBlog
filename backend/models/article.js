254015eccd56de8b4d513340aca6e156ce4bb954
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-04-06 15:27:38 +0200

    add article in model #backend

---
+++ backend/models/article.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const articleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Article', articleSchema)
