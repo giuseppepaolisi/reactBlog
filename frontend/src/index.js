cac8a835d770ddb437e150d5c0bcf7f781aa0975
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-04-11 19:31:58 +0200

    add bootstrap js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
 
 import { Provider } from 'react-redux';
 import { store } from './redux/auth/store';
